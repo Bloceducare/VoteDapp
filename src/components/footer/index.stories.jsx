35a9fec166be6093dad410302477b52ef0d042f3
JanMooMoo <35936674+JanMooMoo@users.noreply.github.com>
2020-10-15 13:11:27 -0700

    Revert "Dapp store v1"

--- src/components/footer/index.stories.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-
-import Footer from '.';
-
-storiesOf('Footer', module)
-  .add('Standard', () => <Footer />);
--- src/components/identicon/index.stories.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-
-import Identicon from '.';
-
-storiesOf('Identicon', module)
-  .add('Standard', () => <Identicon />);
